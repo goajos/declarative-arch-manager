6d27706534a16da514f19286094170a207f4023d
goajos <jappecremers@gmail.com>
2026-02-03 16:11:21 +0100

    clean slate

--- utils/array.c
+++
@@ -1,9 +0,0 @@
-#define array_append(xs, x)\
-    do {\
-        if (xs.count >= xs.capacity) {\
-            if (xs.capacity == 0) xs.capacity = 256;\
-            else xs.capacity *= 2;\
-            xs.items = realloc(xs.items, xs.capacity*sizeof(*xs.items));\
-        }\
-        xs.items[xs.count++] = x;\
-    } while(0)
