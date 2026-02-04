04b66a0e959a3f0ca42ad59313f5b81e3ffdecca
goajos <jappecremers@gmail.com>
2026-02-04 23:30:51 +0100

    reset for stateful implementation

--- commands/init.c
+++
@@ -1,8 +0,0 @@
-#include "../context/context.h"
-
-int damngr_init() {
-    int ret;
-    ret = init_context();
-
-    return ret;
-}
--- context/utils/user.c
+++
@@ -1,7 +0,0 @@
-#include <pwd.h>
-#include <unistd.h>
-
-char* get_user() {
-    struct passwd* pwd = getpwuid(geteuid());
-    return pwd->pw_name;
-}
