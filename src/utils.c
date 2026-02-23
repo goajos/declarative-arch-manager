3187891bbf45f44091da41193efbeb65910a60e2
goajos <jappecremers@gmail.com>
2026-02-23 21:37:23 +0100

    working parse flow for configs

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,7 @@
 #include <pwd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 char *get_user() {
@@ -10,4 +12,15 @@ char *get_user() {
 size_t read_func(void *user_data, char *buf, size_t bufsize) {
   FILE *fid = (FILE *)user_data;
   return fread(buf, 1, bufsize, fid);
+}
+
+char *string_copy(char *str) {
+  char *ret = nullptr;
+  size_t len = strlen(str);
+  if (len) {
+    ret = malloc(len + 1);
+    memcpy(ret, str, len);
+    ret[len] = '\0'; // ensure proper null termination
+  }
+  return ret;
 }
