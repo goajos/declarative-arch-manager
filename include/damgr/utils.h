b15ccd3d433079abdf6421d6d2a086bfd06c3a88
goajos <jappecremers@gmail.com>
2026-03-10 23:29:06 +0100

    setup for actions to be reimplemented

--- include/damgr/utils.h
+++ include/damgr/utils.h
@@ -18,7 +18,20 @@ void damgr_string_trim(char *str);
 
 int damgr_get_conf_key(char *key);
 
+int damgr_qcharcmp(const void *p1, const void *p2);
+
 int damgr_execute_aur_update_command(char *aur_helper);
 int damgr_execute_update_command();
+
+int damgr_execute_package_install_command(struct darray packages);
+int damgr_execute_aur_package_install_command(struct darray packages,
+                                              char *aur_helper);
+int damgr_execute_package_remove_command(struct darray packages);
+// TODO: add a check to see if the shell commands are executable or not
+// TODO: can the shell commands output be supressed?
+int damgr_execute_hook_command(char *user, bool privileged, char *hook);
+int damgr_execute_service_command(bool privileged, bool to_enable,
+                                  char *service);
+int damgr_execute_dotfile_command(char *user, bool to_link, char *service);
 
 #endif /* DAMGR_UTILS_H */
