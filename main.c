c4a0c4d233b5875113c4d1e1d6cdeb0eca87e381
goajos <jappecremers@gmail.com>
2026-09-25 15:30:53 +0200

    working on completing the parse step\nparse config done\nparse active host done

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <kdl/kdl.h>
+#include <glob.h>
 
 #define da_append(xs, x)\
     do {\
@@ -13,22 +14,32 @@
         xs.items[xs.count++] = x;\
     } while(0)
 
+typedef struct {
+    char *item;
+    bool active;
+} Host;
 
 typedef struct {
-    char **items;
+    Host *items;
+    size_t count;
+    size_t capacity;
+} Hosts;
+
+typedef struct {
+    char *item;
+    bool active;
+} Module;
+
+typedef struct {
+    Module *items;
     size_t count;
     size_t capacity;
 } Modules;
 
 typedef struct {
-    char **items;
-    size_t count;
-    size_t capacity;
-} Packages;
-
-typedef struct {
     char *item;
-    bool type; 
+    bool active;
+    bool user_type;
 } Service;
 
 typedef struct {
@@ -39,7 +50,8 @@ typedef struct {
 
 typedef struct {
     char *item;
-    bool type;
+    bool active;
+    bool user_type;
 } Hook;
 
 typedef struct {
@@ -49,165 +61,207 @@ typedef struct {
 } Hooks;
 
 
-static size_t read_func(void* userdata, char* buf, size_t bufsize)
+static size_t read_func(void *user_data, char *buf, size_t bufsize)
 {
-    FILE* fid = (FILE*)userdata;
+    FILE *fid = (FILE *)user_data;
     return fread(buf, 1, bufsize, fid);
 }
 
-char* copy_data(const char* str)
+char *copy_data(const char *str)
 {
     size_t str_size = strlen(str) + 1;
     char *dest = (char *)malloc(str_size);
     if (dest) return strcpy(dest, str);
-    else return NULL;
+    else return nullptr;
 }
 
-void parse_kdl_fid(FILE* fid, Modules* ms, Packages* ps, Services* ss, Hooks* hs)
+void parse_config_kdl(FILE *fid, Hosts *hosts)
 {
-    kdl_parser* parser = kdl_create_stream_parser(&read_func, (void*)fid, KDL_DEFAULTS);
-
-    bool eof = false;
-    int node_depth = 0;
-    char *current_node_type = NULL;
-    // 0 for system, 1 for user
-    bool type_flag;
-    while (1) {
-        kdl_event_data *event = kdl_parser_next_event(parser);
-        const char *event_name = NULL;
-        kdl_event kevent = event->event;
-        const char *data = event->name.data;
-        kdl_type type = event->value.type;
-        const kdl_str *kstring = NULL;
-        kdl_number knum = event->value.number;
-        kdl_number_type ntype = knum.type;
-
-        switch(kevent) {
-            case KDL_EVENT_EOF:
-                event_name = "KDL_EVENT_EOF";
-                // printf("%s\n", event_name);
+   kdl_parser *parser = kdl_create_stream_parser(&read_func, (void *)fid, KDL_DEFAULTS); 
+   
+   bool eof = false;
+   int node_depth = 0;
+   const char *node_data = nullptr;
+   
+   while(1) {
+        kdl_event_data *parsed_event = kdl_parser_next_event(parser);
+        kdl_event event = parsed_event->event;
+        const char *name_data = parsed_event->name.data;
+        kdl_type value_type = parsed_event->value.type;
+        bool boolean = parsed_event->value.boolean;
+    
+        while(1) {
+            if (event == KDL_EVENT_START_NODE) {
+                node_depth += 1;
+                printf("Node depth inc: %d\n", node_depth);
+                if (name_data) printf("Start node: %s\n", name_data);
+                if (node_depth == 1) {
+                    // TODO: set up a goto error block
+                    if (strcmp(name_data, "config") != 0) printf("goto Error: Not a valid config...\n"); 
+                } else if (node_depth == 2) {
+                    node_data = name_data;
+                }
+                break;
+            } else if (event == KDL_EVENT_END_NODE) {
+                node_depth -= 1;
+                printf("Node depth dec: %d\n", node_depth);
+                printf("End node\n");
+                break;
+            } else if (event == KDL_EVENT_ARGUMENT) {
+                if (value_type == KDL_TYPE_BOOLEAN) {
+                    char *dest = copy_data(node_data);
+                    if (dest) {
+                        Host host = { dest, boolean };
+                        da_append((*hosts), host);
+                    }
+                    printf("Event argument node: %b\n", boolean);
+                }
+                break;
+            } else if (event == KDL_EVENT_EOF) {
                 eof = true;
                 break;
-            case KDL_EVENT_START_NODE:
-                event_name = "KDL_EVENT_START_NODE";
-                printf("%s\n", event_name);
-                node_depth += 1;
-                printf("node depth incremented: %d\n", node_depth);
-                if (data) {
-                    printf("%s\n", data);
-                    if (type_flag) printf("%b\n", type_flag);
-                    if (node_depth == 1) {
-                        if (strcmp(data, "host") == 0) type_flag = false; 
-                        else if (strcmp(data, "module") == 0) type_flag = true; 
-                    } else if (node_depth == 2) {
-                        if (strcmp(data, "modules") == 0) current_node_type="modules";
-                        else if (strcmp(data, "packages") == 0) current_node_type="packages";
-                        else if (strcmp(data, "services") == 0) current_node_type="services";
-                        else if (strcmp(data, "hooks") == 0) current_node_type="hooks";
-                    } else if (node_depth == 3) {
-                        if (strcmp(current_node_type, "modules") == 0) {
-                            char* dest = copy_data(data);
-                            if (dest) da_append((*ms), dest);
-                        } else if (strcmp(current_node_type, "packages") == 0) {
-                            char* dest = copy_data(data);
-                            if (dest) da_append((*ps), dest);
-                        } else if (strcmp(current_node_type, "services") == 0) {
-                            char* dest = copy_data(data);
-                            Service s = { dest, type_flag };
-                            da_append((*ss), s);
-                        } else if (strcmp(current_node_type, "hooks") == 0) {
-                            char* dest = copy_data(data);
-                            Hook h = { dest, type_flag };
-                            da_append((*hs), h);
-                        }
-                    }
-                }
-                break;
-            case KDL_EVENT_END_NODE:
-                event_name = "KDL_EVENT_END_NODE";
-                printf("%s\n", event_name);
-                node_depth -= 1;
-                printf("node depth decremented: %d\n", node_depth);
-                if (data) {
-                    printf("%s\n", data);
-                }
-                break;
-            case KDL_EVENT_ARGUMENT:
-                event_name = "KDL_EVENT_ARGUMENT";
-                printf("%s\n", event_name);
-                if (data) printf("%s\n", data);
-                if (type == KDL_TYPE_STRING) {
-                    kstring = &event->value.string;
-                    kdl_owned_string kostring = kdl_clone_str(kstring); 
-                    printf("%s\n", kostring.data);
-                    kdl_free_string(&kostring);
-                } else if (type == KDL_TYPE_NUMBER) {
-                    switch(ntype) {
-                        case KDL_NUMBER_TYPE_INTEGER:
-                            if (knum.integer) printf("%lld\n", knum.integer);
-                        case KDL_NUMBER_TYPE_FLOATING_POINT:
-                            if (knum.floating_point) printf("%f\n", knum.floating_point);
-                        case KDL_NUMBER_TYPE_STRING_ENCODED:
-                            kstring = &event->value.number.string;
-                            kdl_owned_string kostring = kdl_clone_str(kstring);
-                            if (kostring.len != 0) {
-                                printf("%s\n", kostring.data);
-                            }
-                            kdl_free_string(&kostring);
-                    }
-                }
-                break;
-            case KDL_EVENT_COMMENT:
-                event_name = "KDL_EVENT_COMMENT";
-                // printf("%s\n", event_name);
-                break;
-            case KDL_EVENT_PROPERTY:
-                event_name = "KDL_EVENT_PROPERTY";
-                // printf("%s\n", event_name);
-                break;
-            case KDL_EVENT_PARSE_ERROR:
-                event_name = "KDL_EVENT_PARSE_ERROR";
-                // printf("%s\n", event_name);
-                break;
+            }
         }
 
-        if (eof) break;
-    }
+        if (eof) break; // outer while break
+   }
 
-    fclose(fid);
-    kdl_destroy_parser(parser);
+   kdl_destroy_parser(parser); // parser cleans up
 }
 
-// install src/utils/ckdl-cat ~/.local/bin
-// gcc main.c -o main -lkdl -lm -Wall -Wpedantic -Werror
+void parse_host_kdl(FILE *fid, Modules *modules, Services *services, Hooks *hooks)
+{
+   kdl_parser *parser = kdl_create_stream_parser(&read_func, (void *)fid, KDL_DEFAULTS); 
+   
+   bool eof = false;
+   int node_depth = 0;
+   char *node_data_d2 = nullptr;
+   const char *node_data_d3 = nullptr;
+   bool user_type_flag;
+   
+   while(1) {
+        kdl_event_data *parsed_event = kdl_parser_next_event(parser);
+        kdl_event event = parsed_event->event;
+        const char *name_data = parsed_event->name.data;
+        kdl_type value_type = parsed_event->value.type;
+        bool boolean = parsed_event->value.boolean;
+    
+        while(1) {
+            if (event == KDL_EVENT_START_NODE) {
+                node_depth += 1;
+                printf("Node depth inc: %d\n", node_depth);
+                if (name_data) printf("Start node: %s\n", name_data);
+                if (node_depth == 1) {
+                    // TODO: set up a go to error block
+                    if (strcmp(name_data, "host") != 0) printf("goto Error: Not a valid config...\n"); 
+                    else user_type_flag = false; 
+                } else if (node_depth == 2) {
+                    // manual clean up before copying new data
+                    node_data_d2 = copy_data(name_data);
+                } else if (node_depth == 3) {
+                    node_data_d3 = name_data;
+                }
+                break;
+            } else if (event == KDL_EVENT_END_NODE) {
+                node_depth -= 1;
+                printf("Node depth dec: %d\n", node_depth);
+                printf("End node\n");
+                break;
+            } else if (event == KDL_EVENT_ARGUMENT) {
+                if (value_type == KDL_TYPE_BOOLEAN) {
+                    char *dest = copy_data(node_data_d3);
+                    if (dest) {
+                        if (strcmp(node_data_d2, "modules") == 0) {
+                            Module module = { dest, boolean };
+                            da_append((*modules), module);
+                        } else if (strcmp(node_data_d2, "services") == 0) {
+                            Service service = { dest, boolean, user_type_flag };
+                            da_append((*services), service);
+                        } else if (strcmp(node_data_d2, "hooks") == 0) {
+                            Hook hook = { dest, boolean, user_type_flag };
+                            da_append((*hooks), hook);
+                        }
+                    }
+                    printf("Event argument node: %b\n", boolean);
+                }
+                break;
+            } else if (event == KDL_EVENT_EOF) {
+                eof = true;
+                break;
+            }
+        }
+
+        if (eof) break; // outer while break
+   }
+
+   if (node_data_d2 != nullptr) free(node_data_d2); // manual clean up for copied data
+   kdl_destroy_parser(parser); // parser cleans the rest
+}
+
+// gcc main.c -o main -lkdl -lm -std=c23 -Wall -Wextra -Werror -pedantic
 int main()
 {
     printf("Hello world!\n");
-    Modules  ms = {0}; 
-    Packages ps = {0};
-    Services ss = {0};
-    Hooks    hs = {0};
-    
-    FILE* host_fid = fopen("arch.kdl", "r");
-    parse_kdl_fid(host_fid, &ms, &ps, &ss, &hs);
 
-    char fidbuf[0x128];
-    for (int i = 0; i < ms.count; ++i) {
-        snprintf(fidbuf, sizeof(fidbuf), "modules/%s.kdl", ms.items[i]);
-        FILE *module_fid = fopen(fidbuf, "r");  
-        parse_kdl_fid(module_fid, &ms, &ps, &ss, &hs);
+    Hosts hosts = {0};
+
+    FILE *config_fid = fopen("config.kdl", "r");
+    parse_config_kdl(config_fid, &hosts);
+    fclose(config_fid);
+
+    printf("Hosts count: %d\n", (int)hosts.count);
+    char *active_host = nullptr;
+    for (int i = 0; i < (int)hosts.count; ++i) {
+        if (hosts.items[i].active) {
+           active_host = hosts.items[i].item; 
+           break; // only 1 active host assumption
+        }
     }
+    printf("active host: %s\n", active_host);
 
+    glob_t globbuf;
+    globbuf.gl_offs = 0; // not prepending any flags to glob
+    glob("hosts/*.kdl", GLOB_DOOFFS, NULL, &globbuf);
+    char *host_path = nullptr;
+    for (size_t i = 0; i < globbuf.gl_pathc; ++i) {
+        char *glob_path = globbuf.gl_pathv[i];
+        if (strstr(glob_path, active_host) != nullptr) {
+            host_path = copy_data(glob_path);
+            break; // only 1 active host assumption
+        }
+    }
     
-    printf("ms count: %ld\n", ms.count);
-    printf("ps count: %ld\n", ps.count);
-    printf("ss count: %ld\n", ss.count);
-    printf("hs count: %ld\n", hs.count);
+    Modules modules = {0};
+    Services services = {0};
+    Hooks hooks = {0};
+    // if an active host is found
+    if (host_path != nullptr) {
+        char fidbuf[0x256];
+        printf("%s\n", host_path);
+        snprintf(fidbuf, sizeof(fidbuf), "%s", host_path);
+        FILE *host_fid = fopen(fidbuf, "r");  
+        parse_host_kdl(host_fid, &modules, &services, &hooks);
+        fclose(host_fid);
+        
+        printf("Modules count: %d\n", (int)modules.count);
+        for (int i = 0; i < (int)modules.count; ++i) {
+            printf("Module %s - is active: %b\n", modules.items[i].item, modules.items[i].active);
+        }
+    }
+    
+    // no longer need hosts and host glob
+    for (int i = 0; i < (int)hosts.count; ++i) free(hosts.items[i].item);
+    free(hosts.items);
+    globfree(&globbuf);
 
-    for (int i = 0; i < ms.count; ++i) printf("printing ms: %s\n", ms.items[i]);
-    for (int i = 0; i < ps.count; ++i) printf("printing ps: %s\n", ps.items[i]);
-    for (int i = 0; i < ss.count; ++i) printf("printing ss: %s - %b\n", ss.items[i].item, ss.items[i].type);
-    for (int i = 0; i < hs.count; ++i) printf("printing hs: %s - %b\n", hs.items[i].item, hs.items[i].type);
+    // finish the logic for looping over the modules/services/hooks
+
+    for (int i = 0; i < (int)modules.count; ++i) free(modules.items[i].item);
+    free(modules.items);
+    for (int i = 0; i < (int)services.count; ++i) free(services.items[i].item);
+    free(services.items);
+    for (int i = 0; i < (int)hooks.count; ++i) free(hooks.items[i].item);
+    free(hooks.items);
 
     return 0;
 }
