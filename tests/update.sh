5e5fbf432a08669d8316c88039e4e1c789e886f6
goajos <jappecremers@gmail.com>
2026-09-25 15:33:45 +0200

    update.sh update and clean git history

--- tests/update.sh
+++ tests/update.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 DAMGR="./bin/damgr"
+
+echo "----------------------"
 echo "Testing update command"
+echo "----------------------"
+
 $DAMGR update
 
+echo "---------------------"
 echo "Testing update passed"
+echo "---------------------"
