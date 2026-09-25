2d1794c7127a44d9077ea8fe89324bd04d9d6893
goajos <jappecremers@gmail.com>
2026-09-25 15:30:55 +0200

    some basic init and merge testing scripts

--- tests/init.sh
+++ tests/init.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
+set -e # terminate on any failure
 
 DAMGR="./bin/damgr"
-echo "Testing init command"
-$DAMGR init
-test -d ~/.config/damgr || (echo "Testing init failed to create config dir" && exit 1)
-test -d ~/.local/state/damgr || (echo "Testing init failed to create state dir" && exit 1)
+CONFIG_DIR="/home/testuser/.config/damgr"
+STATE_DIR="/home/testuser/.local/state/damgr"
 
+echo "--------------------"
+echo "Testing init command"
+echo "--------------------"
+
+if [ -d $CONFIG_DIR ]; then
+  echo "$CONFIG_DIR already exists"
+  exit 1
+fi
+
+if [ -d $STATE_DIR ]; then
+  echo "$STATE_DIR already exists"
+  exit 1
+fi
+
+$DAMGR init
+
+if [ ! -d $CONFIG_DIR ]; then
+  echo "Testing init failed to create config dir"
+  exit 1
+fi
+
+if [ ! -d $STATE_DIR ]; then
+  echo "Testing init failed to create state dir"
+  exit 1
+fi
+
+echo "-------------------"
 echo "Testing init passed"
+echo "-------------------"
--- tests/merge.sh
+++ tests/merge.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
+set -e # terminate on any failure
 
 DAMGR="./bin/damgr"
-echo "Testing merge command"
-yes | $DAMGR merge
-test -d /home/testuser/.config/nvim || (echo "Testing merge failed to link nvim dotfiles" && exit 1)
-test -O /etc/vimrc || (echo "Testing merge failed to create vimrc with post hook" && exit 1)
+NVIM_DIR="/home/testuser/.config/nvim"
+VIMRC="/etc/vimrc"
 
+echo "---------------------"
+echo "Testing merge command"
+echo "---------------------"
+
+if [ -d $NVIM_DIR ]; then
+  echo "$NVIM_DIR already exists"
+  exit 1
+fi
+
+if [ -f $VIMRC ]; then
+  echo "$VIMRC already exists"
+fi
+
+yes | $DAMGR merge
+
+if [ ! -d $NVIM_DIR ]; then
+  echo "Testing merge failed to link nvim dotfiles"
+  exit 1
+fi
+
+if [ ! -f $VIMRC ]; then
+  echo "Testing merge failed to create vimrc with post hook"
+  exit 1
+fi
+
+echo "--------------------"
 echo "Testing merge passed"
+echo "--------------------"
